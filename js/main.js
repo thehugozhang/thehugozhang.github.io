5f7f9ef80776978ac1475c8d66252bea8e08678b
Hugo Zhang <hugozhang2023@u.northwestern.edu>
2022-12-02 07:37:45 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("nav-link-about").addEventListener("click", () => {
+    document.getElementById("about").scrollIntoView({behavior: "smooth"});
+});
+
+document.getElementById("nav-link-experience").addEventListener("click", () => {
+    document.getElementById("experience").scrollIntoView({behavior: "smooth"});
+});
+
+document.getElementById("nav-link-projects").addEventListener("click", () => {
+    document.getElementById("projects").scrollIntoView({behavior: "smooth"});
+});
